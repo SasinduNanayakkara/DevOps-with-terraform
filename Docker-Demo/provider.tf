9af404ef368e288724067ed287b00b96b7c91838
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-04-11 02:25:11 +0530

    add docker provider

--- Docker-Demo/provider.tf
+++ Docker-Demo/provider.tf
@@ -3,3 +3,15 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 }
+
+# provider "docker" {
+#   host = "unix:///var/run/docker.sock"
+# }
+
+provider "docker" {
+  registry_auth {
+    address = local.aws_ecr_url
+    username = data.aws_ecr_authorization_token.token.username
+    password = data.aws_ecr_authorization_token.token.password
+  }
+}
