033552fdcee5a6e086e57d72db81d3be4855e582
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-04-11 02:24:30 +0530

    add the docker image repo

--- Docker-Demo/ecr.tf
+++ Docker-Demo/ecr.tf
@@ -1,3 +1,11 @@
 resource "aws_ecr_repository" "myapp" {
   name = "myapp"
 }
+
+resource "docker_registry_image" "myapp" {
+  name = "${aws_ecr_repository.myapp.repository_url}/myapp:latest"
+  build{
+    context = "./docker-demo"
+    dockerfile = "Dockerfile"
+  }
+}
